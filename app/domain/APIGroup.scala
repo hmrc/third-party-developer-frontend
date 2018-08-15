3e5cf672db238cccca6c67bfcd480f4b9a282994
peteslater-ee <pslater@equalexperts.com>
2018-08-15 14:23:59 +0100

    GT-1 - Added extracted enums

---
+++ app/domain/APIGroup.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package domain
+
+import enumeratum.{EnumEntry, PlayEnum}
+
+sealed trait APIGroup extends EnumEntry
+
+object APIGroup extends PlayEnum[APIGroup] {
+  val values = findValues
+
+  case object API       extends APIGroup
+  case object TEST_API  extends APIGroup
+  case object EXAMPLE   extends APIGroup
+}
