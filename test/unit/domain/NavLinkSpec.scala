842b71b50da5d9ef4922db1f737be27ecca6b093
Dave O'Brien <41006660+d6fb@users.noreply.github.com>
2019-02-26 16:51:01 +0000

    API-3589 - update navlink specs

--- test/unit/domain/NavLinkSpec.scala
+++ test/unit/domain/NavLinkSpec.scala
@@ -41,7 +41,8 @@ class NavLinkSpec extends UnitSpec {
         Seq(
           NavLink("Documentation", "http://localhost:9680/api-documentation/docs/using-the-hub"),
           NavLink("Applications", "http://localhost:9685/developer/applications"),
-          NavLink("Support", "http://localhost:9685/developer/support"))
+          NavLink("Support", "http://localhost:9685/developer/support"),
+          NavLink("Service availability", "https://api-platform-status.production.tax.service.gov.uk/", openInNewWindow = true))
     }
   }
 }
