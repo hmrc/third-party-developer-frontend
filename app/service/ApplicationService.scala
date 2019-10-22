7d99baba2dc96702015359432407b5a92d73def0
Billy Mitchell <26899143+mitchellb1@users.noreply.github.com>
2019-10-22 16:05:02 +0100

    Fix error when passing email + more tests

--- app/service/ApplicationService.scala
+++ app/service/ApplicationService.scala
@@ -170,10 +170,10 @@ class ApplicationService @Inject()(connectorWrapper: ConnectorsWrapper,
 
     if (environment == Environment.SANDBOX && requesterRole == Role.ADMINISTRATOR && application.access.accessType == AccessType.STANDARD ) {
 
-      applicationConnectorFor(application).deleteApplication(requesterEmail, DeleteApplicationRequest(requester))
+      applicationConnectorFor(application).deleteApplication(application.id, DeleteApplicationRequest(requester))
 
     } else {
-      Future.failed(new ForbiddenException("Developer cannot request to delete a sandbox application"))
+      Future.failed(new ForbiddenException("Only standard subordinate applications can be deleted by admins"))
     }
   }
 
