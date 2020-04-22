28d57435d8cb75609194060e353e5b7bfa4a1f8e
Pete Slater <pslater@equalexperts.com>
2020-04-22 15:15:12 +0100

    APIS-4734 - Added test setup

--- test/config/DevHubAuthWrapperSpec.scala
+++ test/config/DevHubAuthWrapperSpec.scala
@@ -16,8 +16,8 @@
 
 package config
 
-import controllers.{DevHubAuthWrapper, routes}
-import domain.LoggedInState
+import controllers.{routes, DevHubAuthWrapper}
+import domain.{DeveloperSession, LoggedInState}
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.Matchers
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
@@ -29,116 +29,107 @@ import uk.gov.hmrc.play.test.UnitSpec
 import org.mockito.BDDMockito.given
 import uk.gov.hmrc.http.HeaderCarrier
 import org.mockito.ArgumentMatchers.{any, eq => eqTo}
-import utils.{DeveloperSession, SharedMetricsClearDown}
+import utils.{DeveloperSession => DeveloperSessionBuilder, SharedMetricsClearDown}
 import play.api.mvc.Results._
 import play.api.http.Status._
 import play.api.mvc.Cookie
 import play.api.test.Helpers.{defaultAwaitTimeout, redirectLocation}
 
+import cats.implicits._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class DevHubAuthWrapperSpec extends UnitSpec with MockitoSugar with Matchers with GuiceOneAppPerTest with SharedMetricsClearDown {
-  "DebHubAuthWrapper" when {
+  class TestDevHubAuthWrapper(implicit val appConfig: ApplicationConfig) extends DevHubAuthWrapper {
+    override val sessionService: SessionService = mock[SessionService]
+    override val cookieSigner: CookieSigner = fakeApplication.injector.instanceOf[CookieSigner]
+  }
+
+  class Setup(developerSession: Option[DeveloperSession]) {
     implicit val applicationConfig: ApplicationConfig = mock[ApplicationConfig]
     given(applicationConfig.securedCookie).willReturn(false)
-
-    class TestDevHubAuthWrapper(implicit val appConfig: ApplicationConfig) extends DevHubAuthWrapper {
-      override val sessionService: SessionService = mock[SessionService]
-      override val cookieSigner: CookieSigner = fakeApplication.injector.instanceOf[CookieSigner]
-    }
 
     val underTest = new TestDevHubAuthWrapper()
     val sessionId = "sessionId"
 
+    val loggedInAction = underTest.loggedInAction { implicit request =>
+      Future.successful(Ok(EmptyContent()))
+    }
+
+    val atLeastPartLoggedInAction = underTest.atLeastPartLoggedInEnablingMfaAction { implicit request =>
+      Future.successful(Ok(EmptyContent()))
+    }
+
+    val request = FakeRequest().withCookies(underTest.createCookie(sessionId))
+    val requestWithNoCookie = FakeRequest()
+    val requestWithInvalidCookie = FakeRequest().withCookies(Cookie("PLAY2AUTH_SESS_ID","InvalidCookieValue"))
+    val requestWithNoRealSession = FakeRequest().withCookies(underTest.createCookie(sessionId))
+
+    given(underTest.sessionService.fetch(eqTo(sessionId))(any[HeaderCarrier])).willReturn(developerSession.map(ds => ds.session))
+  }
+
+  val loggedInDeveloperSession = DeveloperSessionBuilder("Email", "firstName", "lastName", loggedInState = LoggedInState.LOGGED_IN)
+  val partLoggedInDeveloperSession = DeveloperSessionBuilder("Email", "firstName", "lastName", loggedInState = LoggedInState.PART_LOGGED_IN_ENABLING_MFA)
+
+  "DebHubAuthWrapper" when {
     "the user is logged in and" when {
-      val developerSession = DeveloperSession("Email", "firstName", "lastName", loggedInState = LoggedInState.LOGGED_IN)
-
-      implicit val request = FakeRequest().withCookies(underTest.createCookie(sessionId))
-      given(underTest.sessionService.fetch(eqTo(sessionId))(any[HeaderCarrier])).willReturn(Some(developerSession.session))
-
       "controller action is decorated with loggedInAction" should {
-        "successfully execute action" in {
-          val action = underTest.loggedInAction { implicit request =>
-            Future.successful(Ok(EmptyContent()))
-          }
+        "successfully execute action" in new Setup(loggedInDeveloperSession.some) {
+          val result = await(loggedInAction()(request))
 
-          val result = await(action()(request))
           status(result) shouldBe OK
         }
       }
 
       "controller action is decorated with atLeastPartLoggedInEnablingMfaAction" should {
-        "successfully execute action" in {
-          val action = underTest.atLeastPartLoggedInEnablingMfaAction { implicit request =>
-            Future.successful(Ok(EmptyContent()))
-          }
+        "successfully execute action" in new Setup(loggedInDeveloperSession.some) {
+          val result = await(atLeastPartLoggedInAction()(request))
 
-          val result = await(action()(request))
           status(result) shouldBe OK
         }
       }
     }
 
     "the user is part logged in and" when {
-      val developerSession = utils.DeveloperSession("Email", "firstName", "lastName", loggedInState = LoggedInState.PART_LOGGED_IN_ENABLING_MFA)
-
-      implicit val request = FakeRequest().withCookies(underTest.createCookie(sessionId))
-      given(underTest.sessionService.fetch(eqTo(sessionId))(any[HeaderCarrier])).willReturn(Some(developerSession.session))
-
       "controller action is decorated with loggedInAction" should {
-        "redirect to login page" in {
-          val action = underTest.loggedInAction { implicit request =>
-            Future.successful(Ok(EmptyContent()))
-          }
+        "redirect to login page" in new Setup(partLoggedInDeveloperSession.some) {
+          val result = await(loggedInAction()(request))
 
-          val result = await(action()(request))
           status(result) shouldBe SEE_OTHER
           redirectLocation(result) shouldBe Some(routes.UserLoginAccount.login().url)
         }
       }
 
       "controller action is decorated with atLeastPartLoggedInEnablingMfaAction" should {
-        "successfully execute action" in {
-          val action = underTest.atLeastPartLoggedInEnablingMfaAction { implicit request =>
-            Future.successful(Ok(EmptyContent()))
-          }
+        "successfully execute action" in new Setup(partLoggedInDeveloperSession.some) {
+          val result = await(atLeastPartLoggedInAction()(request))
 
-          val result = await(action()(request))
           status(result) shouldBe OK
         }
       }
     }
 
     "the user is not logged in" when {
-      val action = underTest.atLeastPartLoggedInEnablingMfaAction { implicit request =>
-        Future.successful(Ok(EmptyContent()))
-      }
-
-      "they have no cookie" in {
-        implicit val requestWithNoCookie = FakeRequest()
-
-        val result = await(action()(requestWithNoCookie))
+      "they have no cookie" in new Setup(None) {
+        val result = await(atLeastPartLoggedInAction()(requestWithNoCookie))
 
         status(result) shouldBe SEE_OTHER
         redirectLocation(result) shouldBe Some(routes.UserLoginAccount.login().url)
       }
 
-      "they have a cookie but it is invalid" in {
-        implicit val requestWithInvalidCookie = FakeRequest().withCookies(Cookie("PLAY2AUTH_SESS_ID","InvalidCookieValue"))
+      "they have a cookie but it is invalid" in new Setup(None) {
+        val result = await(atLeastPartLoggedInAction()(requestWithInvalidCookie))
 
-        val result = await(action()(requestWithInvalidCookie))
         status(result) shouldBe SEE_OTHER
         redirectLocation(result) shouldBe Some(routes.UserLoginAccount.login().url)
       }
 
-      "they have a valid cookie but it does not exist in the session service" in {
-        implicit val requestWithNoRealSession = FakeRequest().withCookies(underTest.createCookie(sessionId))
-
+      "they have a valid cookie but it does not exist in the session service" in new Setup(None) {
         given(underTest.sessionService.fetch(eqTo(sessionId))(any[HeaderCarrier]))
           .willReturn(None)
 
-        val result = await(action()(requestWithNoRealSession))
+        val result = await(atLeastPartLoggedInAction()(requestWithNoRealSession))
         status(result) shouldBe SEE_OTHER
         redirectLocation(result) shouldBe Some(routes.UserLoginAccount.login().url)
       }
