e5e73e8740c1d95ca60370f3e020b67ba5df3d90
Rob Dawson <robert.dawson@digital.hmrc.gov.uk>
2021-12-06 14:54:56 +0000

    merging

--- app/modules/submissions/connectors/ThirdPartyApplicationSubmissionsConnector.scala
+++ app/modules/submissions/connectors/ThirdPartyApplicationSubmissionsConnector.scala
@@ -29,11 +29,7 @@ import uk.gov.hmrc.http.HttpReads.Implicits._
 import uk.gov.hmrc.http.UpstreamErrorResponse
 import play.api.libs.json.Json
 import uk.gov.hmrc.play.http.metrics.common.API
-<<<<<<< HEAD
-import scala.util.{Success, Try}
-=======
 import scala.util.{Failure, Success, Try}
->>>>>>> main
 import uk.gov.hmrc.http.HttpResponse
 import play.api.libs.json.JsValue
 
