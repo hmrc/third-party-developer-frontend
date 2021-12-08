2907a6bb4f38695d8aac01866f7021b2c6470a77
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2021-12-08 09:30:25 +0000

    APIS-5557 - Admin/Dev on clicking app in approvals and button on requ… (#478)
    * wip
    
    * APIS-5557 - Admin/Dev on clicking app in approvals and button on requested page now work
    
    * adding tests for ProdCredsChecklistController, minor code fixes
    
    * Add tests for requestApproval
    
    * minor ui fixes
    
    * moving prod creds request page to use devmain2
    
    Co-authored-by: Rob Dawson <robert.dawson@digital.hmrc.gov.uk>
    Co-authored-by: petekirby-ee <pete.kirby@digital.hmrc.gov.uk>

--- app/modules/submissions/controllers/ProdCredsChecklistController.scala
+++ app/modules/submissions/controllers/ProdCredsChecklistController.scala
@@ -145,7 +145,7 @@ class ProdCredsChecklistController @Inject() (
         
         successful(
           filterGroupingsForEmptyQuestionnaireSummaries(viewModel.groupings).fold(
-            BadRequest("No questionnaires applicable") 
+            throw new AssertionError("submissions with only n/a questionnaires will be marked as complete")
           )(vg =>
             Ok(productionCredentialsChecklistView(viewModel.copy(groupings = vg), DummyForm.form.fill(validForm).withGlobalError("production.credentials.checklist.error.global")))
           )
@@ -153,15 +153,8 @@ class ProdCredsChecklistController @Inject() (
       }
     }
 
-    def handleInvalidForm(formWithErrors: Form[DummyForm]) =
-      successful(
-        BadRequest(
-          productionCredentialsChecklistView(
-            convertSubmissionToViewModel(request.extSubmission)(request.application.id, request.application.name),
-            formWithErrors
-          )
-        )
-      )
+    def handleInvalidForm(formWithErrors: Form[DummyForm]) = 
+      throw new AssertionError("DummyForm has no validation rules and so can never be invalid")
 
     DummyForm.form.bindFromRequest.fold(handleInvalidForm, handleValidForm)
   }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,7 @@
 resolvers += "HMRC-open-artefacts-maven" at "https://open.artefacts.tax.service.gov.uk/maven2"
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("uk.gov.hmrc"            %  "sbt-auto-build"         % "3.5.0")
+addSbtPlugin("uk.gov.hmrc"            %  "sbt-auto-build"         % "3.6.0")
 addSbtPlugin("uk.gov.hmrc"            %  "sbt-distributables"     % "2.1.0")
 addSbtPlugin("com.typesafe.play"      %  "sbt-plugin"             % "2.8.7")
 addSbtPlugin("org.irundaia.sbt"       %  "sbt-sassify"            % "1.4.11")
@@ -10,4 +10,4 @@ addSbtPlugin("com.typesafe.sbt"       %  "sbt-uglify"             % "2.0.0")
 addSbtPlugin("com.typesafe.sbt"       %  "sbt-digest"             % "1.1.4")
 addSbtPlugin("org.scoverage"          %  "sbt-scoverage"          % "1.6.0")
 addSbtPlugin("org.scalastyle"         %% "scalastyle-sbt-plugin"  % "1.0.0")
-addSbtPlugin("ch.epfl.scala"          %  "sbt-bloop"              % "1.4.9")
+addSbtPlugin("ch.epfl.scala"          %  "sbt-bloop"              % "1.4.11")
