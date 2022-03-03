263f61697f33ba1ee33f7041eac464bbbc5da079
robdawson-hmrc <94365945+robdawson-hmrc@users.noreply.github.com>
2022-03-03 15:11:22 +0000

    APIS-5663 - wording change for questionnaire

--- test-utils/uk/gov/hmrc/apiplatform/modules/submissions/QuestionnaireTestData.scala
+++ test-utils/uk/gov/hmrc/apiplatform/modules/submissions/QuestionnaireTestData.scala
@@ -128,7 +128,7 @@ trait QuestionnaireTestData {
       Wording("Identify your organisation"),
       Statement(
         List(
-          StatementText("Provide evidence that your organisation is officially registered in the UK."),
+          StatementText("Provide evidence that you or your organisation is officially registered in the UK."),
           StatementText("Choose one option")
         )
       ),
