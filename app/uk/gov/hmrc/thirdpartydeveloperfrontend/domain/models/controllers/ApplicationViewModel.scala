5beeffe54ad634005651a5f8500ec6f4d6aee7b8
robdawson-hmrc <94365945+robdawson-hmrc@users.noreply.github.com>
2022-03-08 15:44:13 +0000

    APIS-5669 - code improvements

--- app/uk/gov/hmrc/thirdpartydeveloperfrontend/domain/models/controllers/ApplicationViewModel.scala
+++ app/uk/gov/hmrc/thirdpartydeveloperfrontend/domain/models/controllers/ApplicationViewModel.scala
@@ -19,12 +19,6 @@ package uk.gov.hmrc.thirdpartydeveloperfrontend.domain.models.controllers
 import uk.gov.hmrc.thirdpartydeveloperfrontend.domain.models.applications.Application
 
 case class ApplicationViewModel(application: Application, hasSubscriptionsFields: Boolean, hasPpnsFields: Boolean) {
-  lazy val privacyPolicyInDesktopApp = application.privacyPolicyUrl match {
-    case Some("desktop") => true
-    case _ => false
-  }
-  lazy val termsAndConditionsInDesktopApp = application.termsAndConditionsUrl match {
-    case Some("desktop") => true
-    case _ => false
-  }
+  lazy val privacyPolicyInDesktopApp = application.privacyPolicyUrl.exists(_ == "desktop")
+  lazy val termsAndConditionsInDesktopApp = application.termsAndConditionsUrl.exists(_ == "desktop")
 }
