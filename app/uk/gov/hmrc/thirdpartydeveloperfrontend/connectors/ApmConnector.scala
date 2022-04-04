4b271904e79b6cf35eb486c3c9230bbb1799fedc
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2022-04-04 13:58:41 +0100

    API-5227: remove deprecated code

--- app/uk/gov/hmrc/thirdpartydeveloperfrontend/connectors/ApmConnector.scala
+++ app/uk/gov/hmrc/thirdpartydeveloperfrontend/connectors/ApmConnector.scala
@@ -78,11 +78,6 @@ class ApmConnector @Inject()(http: HttpClient, config: ApmConnector.Config, metr
     http.GET[Map[ApiContext, ApiData]](s"${config.serviceBaseUrl}/api-definitions/open", Seq("environment" -> environment.toString))
   }
 
-  @deprecated("This is no longer used, please use fetchAllCombinedAPICategories", "")
-  def fetchAllAPICategories()(implicit hc: HeaderCarrier): Future[List[APICategoryDisplayDetails]] =
-    http.GET[List[APICategoryDisplayDetails]](s"${config.serviceBaseUrl}/api-categories")
-
-
   def fetchAllCombinedAPICategories()(implicit hc: HeaderCarrier): Future[Either[Throwable, List[APICategoryDisplayDetails]]] =
     http.GET[List[APICategoryDisplayDetails]](s"${config.serviceBaseUrl}/api-categories/combined")
       .map(Right(_))
