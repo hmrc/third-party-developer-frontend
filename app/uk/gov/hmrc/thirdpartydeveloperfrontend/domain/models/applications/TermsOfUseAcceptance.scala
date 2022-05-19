25477e4c1b1e46a045823a701ad2aa11c9367882
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2022-05-19 08:44:41 +0100

    APIS-5771 TermsOfUseAcceptance changes from TPA (#574)

--- app/uk/gov/hmrc/thirdpartydeveloperfrontend/domain/models/applications/TermsOfUseAcceptance.scala
+++ app/uk/gov/hmrc/thirdpartydeveloperfrontend/domain/models/applications/TermsOfUseAcceptance.scala
@@ -20,7 +20,7 @@ import play.api.libs.json._
 import org.joda.time.{DateTime, DateTimeZone}
 import uk.gov.hmrc.apiplatform.modules.submissions.domain.models.Submission
 
-case class TermsOfUseAcceptance(responsibleIndividual: ResponsibleIndividual, dateTime: DateTime, submissionId: Submission.Id)
+case class TermsOfUseAcceptance(responsibleIndividual: ResponsibleIndividual, dateTime: DateTime, submissionId: Submission.Id, submissionInstance: Int)
 
 object TermsOfUseAcceptance {
   import JodaWrites.JodaDateTimeWrites
