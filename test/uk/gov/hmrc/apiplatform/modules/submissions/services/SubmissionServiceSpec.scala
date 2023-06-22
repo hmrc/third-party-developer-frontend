3975bdb92e3e2dd841b2b3f69208c72ddd3c63e4
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2023-06-22 15:32:37 +0100

    API-6698 PR comments

--- test/uk/gov/hmrc/apiplatform/modules/submissions/services/SubmissionServiceSpec.scala
+++ test/uk/gov/hmrc/apiplatform/modules/submissions/services/SubmissionServiceSpec.scala
@@ -45,7 +45,7 @@ class SubmissionServiceSpec extends AsyncHmrcSpec {
 
       val result = await(underTest.fetch(completelyAnswerExtendedSubmission.submission.id))
 
-      result.isDefined shouldBe true
+      result shouldBe defined
       result.get.submission.id shouldBe completelyAnswerExtendedSubmission.submission.id
     }
 
@@ -54,7 +54,7 @@ class SubmissionServiceSpec extends AsyncHmrcSpec {
 
       val result = await(underTest.fetchLatestSubmission(aSubmission.applicationId))
 
-      result.isDefined shouldBe true
+      result shouldBe defined
       result.get.id shouldBe aSubmission.id
     }
 
@@ -63,7 +63,7 @@ class SubmissionServiceSpec extends AsyncHmrcSpec {
 
       val result = await(underTest.fetchLatestExtendedSubmission(completelyAnswerExtendedSubmission.submission.applicationId))
 
-      result.isDefined shouldBe true
+      result shouldBe defined
       result.get.submission.id shouldBe completelyAnswerExtendedSubmission.submission.id
     }
 
