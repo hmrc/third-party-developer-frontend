26e0cf6c5b50856540cc4ce43c201eb214060fb7
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2023-08-21 09:09:10 +0100

    API-7228 Add escalatedTo optional string to Submission.Granted (#765)

--- test-utils/uk/gov/hmrc/apiplatform/modules/submissions/SubmissionsTestData.scala
+++ test-utils/uk/gov/hmrc/apiplatform/modules/submissions/SubmissionsTestData.scala
@@ -117,7 +117,7 @@ trait SubmissionsTestData extends QuestionBuilder with QuestionnaireTestData wit
   val answeredSubmission  = createdSubmission.hasCompletelyAnsweredWith(answersToQuestions)
   val submittedSubmission = Submission.submit(now, "bob@example.com")(answeredSubmission)
   val declinedSubmission  = Submission.decline(now, gatekeeperUserName, reasons)(submittedSubmission)
-  val grantedSubmission   = Submission.grant(now, gatekeeperUserName, None)(submittedSubmission)
+  val grantedSubmission   = Submission.grant(now, gatekeeperUserName, None, None)(submittedSubmission)
 
   def buildSubmissionWithQuestions(appId: ApplicationId = ApplicationId.random): Submission = {
     val subId = Submission.Id.random
