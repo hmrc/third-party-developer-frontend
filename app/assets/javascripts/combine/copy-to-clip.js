ee5caa1d7f4fd6ace1542c7f162a654ad4a9c29e
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2023-08-30 14:08:39 +0100

    API-7238: Use clipboard API for copy

--- app/assets/javascripts/combine/copy-to-clip.js
+++ app/assets/javascripts/combine/copy-to-clip.js
@@ -2,32 +2,8 @@ $(document).ready(function() {
   // Copy To Clipboard
   var copyButtons = $('.copy-to-clip');
 
-  function copyTextToClipboard(text) {
-    var textArea = document.createElement("textarea");
-    textArea.style.position = 'fixed';
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-    textArea.style.width = '2em';
-    textArea.style.height = '2em';
-    textArea.style.padding = 0;
-    textArea.style.border = 'none';
-    textArea.style.outline = 'none';
-    textArea.style.boxShadow = 'none';
-    textArea.style.background = 'transparent';
-    textArea.value = text;
-
-    document.body.appendChild(textArea);
-
-    textArea.select();
-
-    try {
-      var successful = document.execCommand('copy');
-    } catch (e) {
-      // allow failure - still want to remove textArea
-      // test if we should even display the button later
-    }
-
-    document.body.removeChild(textArea);
+  function copyToClipboard(textToCopy) {
+    navigator.clipboard.writeText(textToCopy);
   }
 
   copyButtons.each(function(index) {
@@ -38,7 +14,7 @@ $(document).ready(function() {
       e.preventDefault();
       e.stopPropagation();
 
-      copyTextToClipboard(self.data('clip-text') );
+      copyToClipboard(self.data('clip-text') );
       self.attr('aria-label', ariaLabel.replace('Copy', 'Copied'));
       self.focus();
 
