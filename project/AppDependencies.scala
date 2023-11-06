c42006aead0112e231dc70c484e4051bb4835056
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-11-06 13:38:16 +0000

    APIS-6544 - Bump lib version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,7 +16,7 @@ object AppDependencies {
   lazy val compile = Seq(
     ws,
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-28"         % bootstrapVersion,
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc"                 % "7.14.0-play-28",
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc"                 % "7.26.0-play-28",
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-play-28"                 % mongoVersion,
     "uk.gov.hmrc"                 %% "http-metrics"                       % "2.7.0",
     "uk.gov.hmrc"                 %% "json-encryption"                    % "5.1.0-play-28",
