a7649745f7ca7cb23d865bfa4add484b35ad278d
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-02-20 00:24:43 +0000

    APIS-6826 Update Tudor crown

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,7 +19,7 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"                 %% "play-partials-play-30"                  % "9.1.0",
     "uk.gov.hmrc"                 %% "domain-play-30"                         % "9.0.0",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"             % "8.4.0",
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"             % "8.5.0",
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-play-30"                     % mongoVersion,
     "uk.gov.hmrc"                 %% "crypto-json-play-30"                    % "7.6.0",
     "uk.gov.hmrc"                 %% "http-metrics"                           % "2.8.0",
