16fbabe36533e6b0dad305c798d8501fa47c8a1f
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-03-21 11:17:29 +0000

    APIS-6949 Fix accordions on API subscriptions page

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -103,5 +103,5 @@
   });
 
   // Overwrite the function responsible for setting the initial accordion closed or open state based on previous interaction.
-  window.GOVUKFrontend.Accordion.prototype.setInitialState = () => {};
+  //window.GOVUKFrontend.Accordion.prototype.setInitialState = () => {};
 })();
