fe69cec8fb62791c60620531121aaae209822f76
peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>
2025-03-31 14:01:51 +0100

    API-8325 - Starting to create a test only route for cloning users

---
+++ app/uk/gov/hmrc/apiplatform/modules/test_only/controllers/TestOnlyUserController.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apiplatform.modules.test_only.controllers
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future.successful
+
+import views.helper.EnvironmentNameService
+
+import play.api.libs.crypto.CookieSigner
+import play.api.libs.json.Json
+import play.api.mvc._
+
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.UserId
+import uk.gov.hmrc.apiplatform.modules.test_only.connectors.TestOnlyTpdConnector
+import uk.gov.hmrc.thirdpartydeveloperfrontend.config.{ApplicationConfig, ErrorHandler}
+import uk.gov.hmrc.thirdpartydeveloperfrontend.controllers.LoggedInController
+import uk.gov.hmrc.thirdpartydeveloperfrontend.service.SessionService
+
+@Singleton
+class TestOnlyUserController @Inject() (
+    connector: TestOnlyTpdConnector,
+    val errorHandler: ErrorHandler,
+    val sessionService: SessionService,
+    val cookieSigner: CookieSigner,
+    mcc: MessagesControllerComponents
+  )(implicit val ec: ExecutionContext,
+    val appConfig: ApplicationConfig,
+    val environmentNameService: EnvironmentNameService
+  ) extends LoggedInController(mcc) {
+
+  def cloneUser(userId: UserId): Action[AnyContent] = Action.async { implicit request =>
+    connector.clone(userId).flatMap {
+      _.fold(
+        errorHandler.notFoundTemplate.map(NotFound(_))
+      )(u => successful(Created(Json.toJson(u))))
+    }
+  }
+}
