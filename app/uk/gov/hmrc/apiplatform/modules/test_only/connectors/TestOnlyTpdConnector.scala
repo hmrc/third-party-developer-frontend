fe7f47f773790f9c45f49b61893e49cb6fce736e
Pete Slater <28756214+peteslater-ee@users.noreply.github.com>
2025-04-01 14:42:41 +0100

    API-8325 - Adding test only routes to support QA (#891)
    * API-8325 - Starting to create a test only route for cloning users
    
    * API-8325 - Further changes
    
    * API-8325 - Excluding test code from coverage
    
    ---------
    
    Co-authored-by: John Green <john.green@digital.hmrc.gov.uk>

---
+++ app/uk/gov/hmrc/apiplatform/modules/test_only/connectors/TestOnlyTpdConnector.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apiplatform.modules.test_only.connectors
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+import play.api.libs.json._
+import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
+
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.{LaxEmailAddress, UserId}
+import uk.gov.hmrc.thirdpartydeveloperfrontend.config.ApplicationConfig
+
+object TestOnlyTpdConnector {
+  case class CloneUserResponse(userId: UserId, emailAddress: LaxEmailAddress)
+
+  implicit val format: OFormat[CloneUserResponse] = Json.format[CloneUserResponse]
+}
+
+@Singleton
+class TestOnlyTpdConnector @Inject() (
+    http: HttpClientV2,
+    config: ApplicationConfig
+  )(implicit val ec: ExecutionContext
+  ) {
+
+  import TestOnlyTpdConnector._
+
+  lazy val serviceBaseUrl: String = config.thirdPartyDeveloperUrl
+
+  def clone(userId: UserId)(implicit hc: HeaderCarrier): Future[CloneUserResponse] = {
+    http.post(url"$serviceBaseUrl/test-only/user/$userId/clone")
+      .execute[CloneUserResponse]
+  }
+}
