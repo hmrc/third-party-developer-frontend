6322c660ba0a1a888a5a2adbac1922f417a61b0e
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-04-07 11:26:47 +0100

    API-8346 - More work

--- app/uk/gov/hmrc/apiplatform/modules/test_only/connectors/TestOnlyTpaConnector.scala
+++ app/uk/gov/hmrc/apiplatform/modules/test_only/connectors/TestOnlyTpaConnector.scala
@@ -19,28 +19,69 @@ package uk.gov.hmrc.apiplatform.modules.test_only.connectors
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
+import uk.gov.hmrc.apiplatformmicroservice.common.utils.EbridgeConfigurator
 import uk.gov.hmrc.http.HttpReads.Implicits._
-import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.client.{HttpClientV2, RequestBuilder}
 import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
 
-import uk.gov.hmrc.thirdpartydeveloperfrontend.config.ApplicationConfig
-import uk.gov.hmrc.apiplatform.modules.common.domain.models.ApplicationId
 import uk.gov.hmrc.apiplatform.modules.applications.core.domain.models.ApplicationWithCollaborators
-import uk.gov.hmrc.apiplatform.modules.common.domain.models.Environment
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.{ApplicationId, Environment}
+import uk.gov.hmrc.thirdpartydeveloperfrontend.config.ApplicationConfig
 
-@Singleton
-class TestOnlyTpaConnector @Inject() (
+abstract class TestOnlyTpaConnector @Inject() (
     http: HttpClientV2,
     config: ApplicationConfig
   )(implicit val ec: ExecutionContext
   ) {
 
+  val environment: Environment
+  val serviceBaseUrl: String
+  def isEnabled: Boolean
+
+  def configureEbridgeIfRequired: RequestBuilder => RequestBuilder
+
   def clone(environment: Environment)(appId: ApplicationId)(implicit hc: HeaderCarrier): Future[ApplicationWithCollaborators] = {
-    val baseUrl = environment match {
-      case Environment.PRODUCTION => config.thirdPartyApplicationProductionUrl
-      case _ => config.thirdPartyApplicationSandboxUrl
+    if (isEnabled) {
+      val url = s"$serviceBaseUrl/developer/applications/test-only/applications"
+
+      configureEbridgeIfRequired(
+        http.post(url"$url/$appId/clone")
+      )
+        .execute[ApplicationWithCollaborators]
+    } else {
+      throw new IllegalArgumentException("Cannot clone sandbox apps when sandbox is disabled")
     }
-    http.post(url"$baseUrl/test-only/application/$appId/clone")
-      .execute[ApplicationWithCollaborators]
   }
+}
+
+@Singleton
+class TestOnlyTpaSandboxConnector @Inject() (
+    val http: HttpClientV2,
+    val appConfig: ApplicationConfig
+  )(implicit ec: ExecutionContext
+  ) extends TestOnlyTpaConnector(http, appConfig)(ec) {
+
+  val environment: Environment = Environment.SANDBOX
+  val serviceBaseUrl: String   = appConfig.thirdPartyApplicationSandboxUrl
+  val useProxy: Boolean        = appConfig.thirdPartyApplicationSandboxUseProxy
+  val apiKey: String           = appConfig.thirdPartyApplicationSandboxApiKey
+
+  lazy val configureEbridgeIfRequired: RequestBuilder => RequestBuilder =
+    EbridgeConfigurator.configure(useProxy, apiKey)
+
+  override val isEnabled: Boolean = appConfig.hasSandbox;
+}
+
+@Singleton
+class TestOnlyTpaProductionConnector @Inject() (
+    val http: HttpClientV2,
+    val appConfig: ApplicationConfig
+  )(implicit ec: ExecutionContext
+  ) extends TestOnlyTpaConnector(http, appConfig)(ec) {
+  val environment: Environment = Environment.PRODUCTION
+  val serviceBaseUrl: String   = appConfig.thirdPartyApplicationProductionUrl
+
+  val configureEbridgeIfRequired: RequestBuilder => RequestBuilder = identity
+
+  override val isEnabled = true
 }
