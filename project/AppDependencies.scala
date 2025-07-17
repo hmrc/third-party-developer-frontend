b22f7fdecfb15e7ef5f6ad303288279780e53826
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-07-17 11:58:50 +0100

    API-8487 - Remove reference to snapshot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   lazy val bootstrapVersion = "9.14.0"
   lazy val mongoVersion = "2.6.0"
   lazy val apiDomainVersion = "0.19.1"
-  lazy val appDomainVersion = "0.82.0-SNAPSHOT"
+  lazy val appDomainVersion = "0.81.0"
   lazy val tpdDomainVersion = "0.13.0"
 
   lazy val compile = Seq(
