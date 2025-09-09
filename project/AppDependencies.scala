00f4c5c9b302d7055f8752cf9e4afec9290e85f8
peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>
2025-09-09 14:08:44 +0100

    API-8606 - PR Bot stuff

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   def apply() : Seq[ModuleID] = compile ++ test
 
   lazy val seleniumVersion = "4.14.0"
-  lazy val bootstrapVersion = "9.18.0"
+  lazy val bootstrapVersion = "9.19.0"
   lazy val mongoVersion = "2.7.0"
   lazy val apiDomainVersion = "0.19.1"
   lazy val appDomainVersion = "0.82.0"
@@ -16,9 +16,9 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"                 %% "play-partials-play-30"                  % "10.1.0",
     "uk.gov.hmrc"                 %% "domain-play-30"                         % "11.0.0",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"             % "12.7.0",
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"             % "12.9.0",
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-play-30"                     % mongoVersion,
-    "uk.gov.hmrc"                 %% "crypto-json-play-30"                    % "8.2.0",
+    "uk.gov.hmrc"                 %% "crypto-json-play-30"                    % "8.3.0",
     "uk.gov.hmrc"                 %% "http-metrics"                           % "2.9.0",
     "uk.gov.hmrc"                 %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "commons-net"                 %  "commons-net"                            % "3.6",
