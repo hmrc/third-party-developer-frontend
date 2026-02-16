90119f05bedb38c6a0413bd636716e7a85c03be4
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-02-16 13:30:07 +0000

    APIS-7764 PR bot action

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,7 +19,7 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"                 %% "play-partials-play-30"                  % "10.2.0",
     "uk.gov.hmrc"                 %% "domain-play-30"                         % "11.0.0",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"             % "12.29.0",
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"             % "12.31.0",
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-play-30"                     % mongoVersion,
     "uk.gov.hmrc"                 %% "crypto-json-play-30"                    % "8.4.0",
     "uk.gov.hmrc"                 %% "http-metrics"                           % "2.9.0",
