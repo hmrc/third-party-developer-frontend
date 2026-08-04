f5cde40df075c37cd49b490bbd52f1e76840115c
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-08-04 15:11:22 +0100

    APIS-8257 Update to latest version of organisation domain library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
   lazy val apiDomainVersion = "1.4.0"
   lazy val appDomainVersion = "1.3.0"
   lazy val tpdDomainVersion = "0.15.0"
-  private val orgDomainVersion = "1.3.0"
+  private val orgDomainVersion = "1.4.0"
   private val mockitoScalaVersion = "2.0.0"
 
 
