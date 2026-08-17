c041318e7ab1a6c335f3bdacf6fd12bdd7904a87
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-08-17 13:54:00 +0100

    APIS-8307 Upgrade to latest hmrc play frontend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,7 +17,7 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-partials-play-30"                 % "10.2.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.11.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
     "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.4.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
